8e4059f284e28212f09f83a124f268bee3ad4509
xfz11 <81600993+xfz11@users.noreply.github.com>
2021-06-22 14:35:26 +0800

    Update settings.py

--- web_project/settings.py
+++ web_project/settings.py
@@ -80,13 +80,28 @@ WSGI_APPLICATION = 'web_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'testDB',
+        'USER': 'zxf@testmysqlzxf',
+        'PASSWORD': 'zxcv231!',
+        'HOST': 'testmysqlzxf.mysql.database.azure.com',
+        'PORT': '3306',
+        'OPTIONS': {
+            'ssl': {
+                'sslmode': 'require'
+            }
+        }
+        
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
